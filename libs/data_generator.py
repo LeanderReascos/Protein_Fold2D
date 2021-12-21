7f7217443d3dfb8e10b023b1dc3f4d9656a91d58
Leander Reascos <reascoslea@gmail.com>
2021-12-21 22:36:43 +0000

    Update

--- libs/data_generator.py
+++ libs/data_generator.py
@@ -12,32 +12,15 @@ def verifica_string(lista,tamanho):
                 count_H +=1
             elif (lista[i][j] == 'P'):
                 count_P +=1
-        if (count_P >=1 and count_H >= tamanho/2):
+        if (count_P >=1 and count_H >= 3):
             result.append(lista[i])
         count_H = 0
         count_P = 0
     return result
 
 
-def gera_inputs():
-
-    a = list(itertools.product('HP', repeat = 5))
-    b = list(itertools.product('HP', repeat = 6))
-    c = list(itertools.product('HP', repeat = 7))
-
-    inputs_5chars = verifica_string(a,5)
-    inputs_6chars = verifica_string(b,6)
-    inputs_7chars = verifica_string(c,7)
-
-    inputs_total = [*inputs_5chars,*inputs_6chars,*inputs_7chars]
-
-    inputs_total = [''.join(x) for x in inputs_total]
-
-    print(inputs_total)
-
-
-def gera_inputs_V2(N): #Recebe uma lista com os comprimentos das strings dos inputs
-    lista = []         #Faz o mesmo que a gera_inputs() mas permite gerar os inputs com os caracteres que quisermos
+def gera_inputs(N): 
+    lista = []         
     for i in range(len(N)):
         lista.append(verifica_string(list(itertools.product('HP',repeat = N[i])),N[i]))
     lista_total = [i for sublist in lista for i in sublist]
@@ -52,7 +35,7 @@ def gera_gerador(input): #Recebe uma string e gera todas as possíveis combinaç
     return a_str
 
 def generate_InputDataset(values):
-    inputs = gera_inputs_V2(values)
+    inputs = gera_inputs(values)
     Inputs = []
     Directions = []
     for i in inputs:
@@ -77,12 +60,19 @@ def generate_Dataset(df):
         exp = make_experiment(I[i],D[i])
         if exp is not None:
             exp.count_energy()
-            energies.append(exp.energy)
-            inputs.append(I[i])
-            directions.append(D[i])
+            if exp.energy > 0:
+                energies.append(exp.energy)
+                inputs.append(I[i])
+                directions.append(D[i])
     final = pd.DataFrame()
     final['Inputs'] = inputs
     final['Directions'] = directions
     final['Energy'] = energies
     return final
 
+if __name__ == '__main__':
+    args = [5,6,7,8]
+    df = generate_InputDataset(args)
+    print(len(df))
+    final = generate_Dataset(df)
+    final.to_csv('data/dataset.csv',index = False)
