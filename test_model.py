cbdb890993838d963297685590f5bbb47cca8d5f
Leander Reascos <reascoslea@gmail.com>
2021-12-28 17:52:11 +0000

    Add test model

---
+++ test_model.py
@@ -0,0 +1,11 @@
+from models.decision_tree import model as dt
+from models.model1 import model as m1
+from libs.functions import make_experiment
+
+
+# Funcao (model,Input,N)
+# ex = make_experiment(Input, resultado)
+#  if exp is not None:
+#       ex.count_energy()
+#       ex.energy()
+# max_energy, reults = [[Direcoes,Energia]]
